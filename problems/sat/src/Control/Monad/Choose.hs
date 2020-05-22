5dd31a01d497a44b3ce639cbc870d259f8e76a5c
Jakob Rath <git@jakobrath.eu>
2020-05-22 12:07:11 +0200

    Make generator smarter (prune some unsuitable branches), add proper backtracking to random generation, allow enumeration of sample space

---
+++ problems/sat/src/Control/Monad/Choose.hs
@@ -0,0 +1,158 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Control.Monad.Choose where
+
+-- base
+import Control.Applicative
+import Data.Functor.Identity
+
+-- combinat
+import Math.Combinat.Permutations
+
+-- list-transformer
+import List.Transformer
+
+-- mtl
+import Control.Monad.Reader
+import Control.Monad.State.Strict
+
+-- random
+import System.Random hiding (next)
+
+
+
+class MonadPlus m => MonadChoose m where
+  choose :: [a] -> m a
+
+-- NOTE: don't use this instance, the built-in list monad seems to build the full list in memory before returning
+-- instance MonadChoose [] where
+--   {-# INLINE choose #-}
+--   choose = id
+
+instance MonadChoose m => MonadChoose (StateT s m) where
+  {-# INLINE choose #-}
+  choose = lift . choose
+
+instance MonadChoose m => MonadChoose (ReaderT r m) where
+  {-# INLINE choose #-}
+  choose = lift . choose
+
+
+{-
+
+newtype RandomChoiceT g m a = RandomChoiceT { unRandomChoiceT :: StateT g m a }
+  deriving newtype (Functor, Applicative, Alternative, Monad, MonadPlus, MonadTrans)
+
+runRandomChoiceT :: RandomChoiceT g m a -> g -> m (a, g)
+runRandomChoiceT r g = runStateT (unRandomChoiceT r) g
+
+randomRM :: (Monad m, RandomGen g, Random a) => (a, a) -> RandomChoiceT g m a
+randomRM range = do
+  g <- RandomChoiceT get
+  let (x, g') = randomR range g
+  RandomChoiceT (put g')
+  return x
+
+instance (MonadPlus m, RandomGen g) => MonadChoose (RandomChoiceT g m) where
+  choose [] =
+    mzero
+  choose [x] =
+    return x
+  choose xs = do
+    i <- randomRM (0, length xs - 1)
+    return (xs !! i)
+
+
+-- RandomChoiceT using the global random generator
+type RandomIO {- a -} = RandomChoiceT StdGen IO {- a -}
+
+evalRandomIO :: RandomChoiceT StdGen IO a -> IO a
+evalRandomIO r = do
+  g <- getStdGen
+  (x, g') <- runRandomChoiceT r g
+  setStdGen g'
+  return x
+
+-- evalRandomIO' :: RandomChoiceT StdGen Identity a -> IO a
+-- evalRandomIO' r = do
+--   g <- getStdGen
+--   let Identity (x, g') = runRandomChoiceT r g
+--   setStdGen g'
+--   return x
+
+-}
+
+
+
+
+instance Monad m => MonadChoose (ListT m) where
+  {-# INLINE choose #-}
+  choose = select
+
+
+
+-- | Improved version of 'RandomChoiceT' that supports proper backtracking.
+newtype RandomListT g m a = RandomListT { unRandomListT :: ListT (StateT g m) a }
+  deriving newtype (Functor, Applicative, Alternative, Monad, MonadPlus)
+
+instance MonadTrans (RandomListT g) where
+  {-# INLINE lift #-}
+  lift = RandomListT . lift . lift
+
+instance (Monad m, RandomGen g) => MonadChoose (RandomListT g m) where
+  {-# INLINE choose #-}
+  choose [] =
+    mzero
+  choose [x] =
+    return x
+  choose xs = do
+    let n = length xs
+    p <- withRandomGen (randomPermutation n)
+    let xs' = permuteList p xs
+    select xs'
+
+{-# INLINE withRandomGen #-}
+withRandomGen :: Monad m => (g -> (a, g)) -> RandomListT g m a
+withRandomGen = RandomListT . lift . state
+
+runRandomListT :: Monad m => RandomListT g m a -> g -> m (Maybe a, g)
+runRandomListT r g =
+  flip runStateT g $ do
+    s <- next (unRandomListT r)
+    case s of
+      Nil -> return Nothing
+      Cons x _ -> return (Just x)
+
+evalRandomListIO :: Monad m => (forall x. m x -> IO x) -> RandomListT StdGen m a -> IO (Maybe a)
+evalRandomListIO unlift r = do
+  g <- getStdGen
+  (x, g') <- unlift (runRandomListT r g)
+  setStdGen g'
+  return x
+
+evalRandomListIO' :: RandomListT StdGen Identity a -> IO (Maybe a)
+evalRandomListIO' = evalRandomListIO (pure . runIdentity)
+
+
+
+
+
+blah :: MonadChoose m => StateT Int m String
+blah = do
+  k <- get
+  x <- choose [k .. k + 5]
+  return (show x)
+
+-- allBlah :: Int -> [String]
+-- allBlah = evalStateT (blah @[])
+
+allBlah' :: Int -> [String]
+allBlah' = runIdentity . List.Transformer.fold (flip (:)) [] reverse . evalStateT (blah @(ListT Identity))
+
+randomBlah :: Int -> IO (Maybe String)
+randomBlah = evalRandomListIO' . evalStateT (blah @(RandomListT StdGen Identity))
+-- randomBlah = evalRandomIO . evalStateT (blah @RandomIO)
--- problems/sat/src/Exagen.hs
+++ problems/sat/src/Exagen.hs
@@ -1,71 +1,304 @@
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Exagen where
 
 -- base
+import Control.Exception (assert)
 import Control.Monad
 import Data.Foldable
+import Data.Functor.Identity
 
 -- containers
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 
+-- list-transformer
+import List.Transformer as ListT
+
+-- mtl
+-- import Control.Monad.Reader
+import Control.Monad.State.Strict
+
 -- prettyprinter
 import Data.Text.Prettyprint.Doc
 import Data.Text.Prettyprint.Doc.Render.String
 
--- QuickCheck
-import Test.QuickCheck
-
 -- exagen
-import Logic.Propositional.Formula
+import Control.Monad.Choose
+import Logic.Propositional.Formula hiding (Prop(..))
+
+
+data Pair a b = Pair !a !b
 
 
 main :: IO ()
 main = do
-  fms <- replicateM 10 generateFormula
+  fms <- replicateM 10 randomExamFormula
   forM_ fms $ \fm -> do
     putStrLn $ showPretty fm
     -- putStrLn $ "Proper Subformulas:\n" <> showPretty (properSubformulas fm)
     putStrLn $ "Polarities: " <> show (atomPolarity fm)
     putStrLn ""
 
+  {-
+  let Pair numFormulas numSuitableFormulas :: Pair Int Int =
+        runIdentity $
+        ListT.fold (\(Pair n k) fm -> Pair (n+1) (if suitable fm then k+1 else k)) (Pair 0 0) id $
+        -- ListT.fold (\(Pair n k) fm -> Pair (n+1) (if suitable fm then assert (suitableFull fm) (k+1) else assert (not $ suitableFull fm) k)) (Pair 0 0) id $
+        genExamFormula
+  putStrLn $ "Size of suitable sample space: " <> show numSuitableFormulas
+  putStrLn $ "Size of sample space: " <> show numFormulas
+  -}
 
-generateFormula :: IO (Formula Prop)
-generateFormula = go 1000
+  -- Results for size = 5:
+  -- Size of suitable sample space: 450240
+  -- Size of sample space: 1767744
+  --
+  -- Results for size = 6:
+  -- Size of suitable sample space: 22999440
+  -- Size of sample space: 96837120
+  --
+  -- Results for size = 7:
+  -- (aborted because it takes too long)
+
+
+-- Only checks those criteria from 'suitableFull' that haven't been inlined in the generator
+suitable :: Ord a => Formula a -> Bool
+suitable fm =
+  foldl1 (&&)
+  [ numAtoms fm == 3
+  , -- there is at least one atom with pure polarity
+    hasAtomPolarity Neg fm || hasAtomPolarity Pos fm
+  -- , -- at least one but at most two equivalences
+  --   let n = countSubformulas isIff fm
+  --   in 1 <= n && n <= 2
+  -- , anySubformula isImp fm
+  -- , anySubformula isNot fm
+  -- , anySubformula isAnd fm || anySubformula isOr fm
+  , not (anySubformula isNestedNot fm)
+  -- , not (anySubformula isTrivialBinaryConnective fm)
+  ]
+
+suitableFull :: Ord a => Formula a -> Bool
+suitableFull fm =
+  foldl1 (&&)
+  [ numAtoms fm == 3
+  , -- there is at least one atom with pure polarity
+    hasAtomPolarity Neg fm || hasAtomPolarity Pos fm
+  , -- at least one but at most two equivalences
+    let n = countSubformulas isIff fm
+    in 1 <= n && n <= 2
+  , anySubformula isImp fm
+  , anySubformula isNot fm
+  , anySubformula isAnd fm || anySubformula isOr fm
+  , not (anySubformula isNestedNot fm)
+  , not (anySubformula isTrivialBinaryConnective fm)
+  ]
+
+
+
+data Prop = P | Q | R
+  deriving (Eq, Ord)
+
+instance Show Prop where
+  show P = "p"
+  show Q = "q"
+  show R = "r"
+
+instance Pretty Prop where
+  pretty = pretty . show
+
+
+randomExamFormula :: IO (Formula Prop)
+randomExamFormula = go 1000
   where
-    genProp = do
-      str <- elements ["p", "q", "r"]
-      return (P str)
-
-    numConnectives = 7
-
     go :: Int -> IO (Formula Prop)
     go 0 = error "max tries exceeded"
     go n = do
-      fm <- generate (resize numConnectives $ genFormula genProp)
-      if suitable fm
-        then return fm
-        else go (n - 1)
+      maybeFm <- evalRandomListIO' genExamFormula
+      case maybeFm of
+        Nothing -> error "empty sample space"
+        Just fm | suitable fm -> assert (suitableFull fm) $ return fm
+        Just fm -> assert (not $ suitableFull fm) $ go (n - 1)
 
-    suitable :: Ord a => Formula a -> Bool
-    suitable fm =
-      foldl1 (&&)
-      [ numAtoms fm == 3
-      , -- there is at least one atom with pure polarity
-        hasAtomPolarity Neg fm || hasAtomPolarity Pos fm
-      , -- at least one but at most two equivalences
-        let n = countSubformulas isIff fm
-        in 1 <= n && n <= 2
-      , anySubformula isImp fm
-      , anySubformula isNot fm
-      , anySubformula isAnd fm || anySubformula isOr fm
-      , not (anySubformula isNestedNot fm)
-      , not (anySubformula isTrivialBinaryConnective fm)
-      ]
+-- NOTE: don't actually use this as it will keep the whole list in memory
+-- allExamFormulas :: ListT Identity (Formula Prop)
+-- allExamFormulas = filter suitable (genExamFormula @(ListT Identity))
+
+
+genExamProp :: MonadChoose m => m Prop
+genExamProp = choose [P, Q, R]
+
+
+genExamFormula :: MonadChoose m => m (Formula Prop)
+genExamFormula = evalStateT (genFormulaPruned size genExamProp) initialState
+  where
+    size = 7
+    initialState = GenState
+      { requiredConnectives = [[CImp], [CIff], [CNot], [CAnd, COr]]
+      , remainingConnectives = size
+      , allowedIff = 2
+      }
+
+data Connective = CNot | CAnd | COr | CImp | CIff
+  deriving (Eq, Ord, Show)
+
+type GenT m = StateT GenState m
+
+data GenState = GenState
+  { -- NOTE: in current implementation, all constraints (type [Connective]) must be pairwise disjoint!
+    requiredConnectives :: [[Connective]]
+  , -- How many connectives are still to be generated in the whole formula (not the subformula size)
+    remainingConnectives :: Int
+  , -- How many equivalences are still allowed in the whole formula
+    allowedIff :: Int
+  }
+
+genFormulaPruned :: forall m a. (Eq a, MonadChoose m) => Int -> GenT m a -> GenT m (Formula a)
+genFormulaPruned totalSize genProp = formula totalSize
+  where
+    formula :: Int -> GenT m (Formula a)
+    formula 0 = base
+    formula n = recursive n
+
+    base :: GenT m (Formula a)
+    base = Atomic <$> genProp
+
+    recursive :: Int -> GenT m (Formula a)
+    recursive n = do
+      numRemaining <- gets remainingConnectives
+      numConstraints <- gets (length . requiredConnectives)
+      when (numRemaining < numConstraints) $ error "bug: remaining < constraints"
+      availableConnectives <-
+        if numRemaining <= numConstraints
+        then concat <$> gets requiredConnectives
+        else do numAllowedIff <- gets allowedIff
+                if numAllowedIff > 0
+                  then return [CNot, CAnd, COr, CImp, CIff]
+                  else return [CNot, CAnd, COr, CImp]
+      conn <-
+        choose availableConnectives
+      modify' $ \s -> s{ requiredConnectives = filter (all (/=conn)) (requiredConnectives s)
+                       , remainingConnectives = remainingConnectives s - 1
+                       , allowedIff = allowedIff s - (if conn == CIff then 1 else 0)
+                       }
+
+      case conn of
+        CNot -> unaryConnective n Not
+        CAnd -> binaryConnective n And
+        COr  -> binaryConnective n Or
+        CImp -> binaryConnective n Imp
+        CIff -> binaryConnective n Iff
+
+    unaryConnective :: Int -> (Formula a -> Formula a) -> GenT m (Formula a)
+    unaryConnective n f = do
+      inner <- formula (n - 1)
+      -- let fm = f inner
+      -- -- TODO: generates whole subformula before check
+      -- if isNestedNot fm
+      --   then mzero
+      --   else return fm
+      return (f inner)
+
+    binaryConnective :: Int -> (Formula a -> Formula a -> Formula a) -> GenT m (Formula a)
+    binaryConnective n f = do
+      let n' = n - 1
+      leftSize <- choose [0 .. n']
+      let rightSize = n' - leftSize
+      left <- formula leftSize
+      right <- formula rightSize
+      let fm = f left right
+      if isTrivialBinaryConnective fm
+        then mzero
+        else return fm
+{-
+genExamFormula :: MonadChoose m => m (Formula Prop)
+genExamFormula = evalStateT (runReaderT (myGenFormula genExamProp) initialInfo) initialState
+  where
+    initialInfo = GenInfo
+      { size = 4  -- TODO
+                  -- , allowedConnectives = error "TODO"
+                  -- , requiredConnectives = [[CImp], [CIff], [CNot], [CAnd, COr]]
+      }
+    initialState = GenState
+      { requiredConnectives = [[CImp], [CIff], [CNot], [CAnd, COr]]
+      }
+
+data GenInfo = GenInfo
+  { size :: Int
+  -- , -- | how many of each connective can still be used
+  --   allowedConnectives :: Map Connective Int
+  -- , requiredConnectives :: [[Connective]]
+  }
+
+data GenState = GenState
+  { requiredConnectives :: [[Connective]]
+  }
+
+-- myResize :: MonadState GenState m => Int -> m a -> m a
+-- myResize newSize mx = do
+--   oldState <- gets size
+--   modify' $ \s -> s{ size = newSize }
+--   x <- mx
+--   modify' $ \s -> s{ size = newSize }
+
+myResize :: MonadReader GenInfo m => Int -> m a -> m a
+myResize newSize = local $ \r -> r{ size = newSize }
+
+myScale :: MonadReader GenInfo m => (Int -> Int) -> m a -> m a
+myScale f = local $ \r -> r{ size = f (size r) }
+
+type GenT m = ReaderT GenInfo (StateT GenState m)
+
+myGenFormula :: forall m a. (Eq a, MonadChoose m) => GenT m a -> GenT m (Formula a)
+myGenFormula genProp = formula
+  where
+    formula :: GenT m (Formula a)
+    formula = do
+      n <- asks size
+      case n of
+        0 -> base
+        _ -> recursive
+
+    base :: GenT m (Formula a)
+    base = Atomic <$> genProp
+
+    recursive :: GenT m (Formula a)
+    recursive = do
+      m <- choose [ unaryConnective Not
+                  , binaryConnective And
+                  , binaryConnective Or
+                  , binaryConnective Imp
+                  , binaryConnective Iff
+                  ]
+      m
+
+    unaryConnective :: (Formula a -> Formula a) -> GenT m (Formula a)
+    unaryConnective f = do
+      inner <- myScale (subtract 1) formula
+      let fm = f inner
+      if isNestedNot fm
+        then mzero
+        else return fm
+
+    binaryConnective :: (Formula a -> Formula a -> Formula a) -> GenT m (Formula a)
+    binaryConnective f = do
+      n <- asks size
+      let n' = n - 1
+      leftSize <- choose [0 .. n']
+      let rightSize = n' - leftSize
+      left <- myResize leftSize formula
+      right <- myResize rightSize formula
+      let fm = f left right
+      if isTrivialBinaryConnective fm
+        then mzero
+        else return fm
+-}
 
 
 -- Return true for binary connectives where both arguments are a literal using the same atom.
