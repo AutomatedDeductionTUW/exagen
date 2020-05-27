7793a1b15d08c025e65f22b4bb9728d62a75f440
Jakob Rath <git@jakobrath.eu>
2020-05-27 19:58:59 +0200

    Add compile scripts

---
+++ exams/clean.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+# Abort on errors
+set -euo pipefail
+
+
+rm **/*.aux **/*.fdb_latexmk **/*.fls **/*.log **/*.out **/*.pdf **/*.synctex.gz
---
+++ exams/compile.zsh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+# Abort on errors
+set -euo pipefail
+
+
+for exam in exam-*; do
+    echo "Compiling $exam"
+
+    ln -sf "../template.tex" "$exam/$exam.tex"
+    ( cd "$exam" && latexmk -quiet -pdf "$exam.tex" )
+done
+
+mkdir -p final
+cp exam-*/exam-*.pdf final/
+
+echo
+echo "Compiled exams are in directory 'final'"
