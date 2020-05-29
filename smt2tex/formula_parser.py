9dbf6a8aa52d07587bbccd999ffcaba7ad1ad1c6
Jakob Rath <git@jakobrath.eu>
2020-05-29 16:23:03 +0200

    Add executable script, move to subdir

---
+++ smt2tex/formula_parser.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import re
+
+mapped_symbols = {
+    "add": "+",
+    "leq": r"\le",
+    "app": r"\append"
+}
+
+# value means parenthesis is
+# potentially needed around args
+infix_symbols = {
+    "+": True,
+    "=": False,
+    r"\le": False,
+    r"\append": True
+}
+
+def prettify_var(var):
+    m = re.match(r".*?([0-9]+)$", var)
+    if not m:
+        return var
+    ind = m.start(1)
+    if ind == 0:
+        return var
+    else:
+        return var[:ind]+"_{"+var[ind:]+"}"
+
+def get_symbol(token):
+    if token in mapped_symbols:
+        return mapped_symbols[token]
+    return token
+
+def handle_forall(vars):
+    res = r"\forall "
+    for v in vars[:-1]:
+        res += prettify_var(v[0]) + ","
+    res += prettify_var(vars[-1][0]) + "."
+    return res
+
+def parse_terms(terms):
+    res = ""
+    for t in terms[:-1]:
+        res += parse(t)[0] + ","
+    res += parse(terms[-1])[0]
+    return res
+
+def parse(expr):
+    result = ""
+    p_outside = False
+    if not isinstance(expr, list):
+        return prettify_var(expr), False
+    symbol = get_symbol(expr[0])
+    if len(expr) == 1:
+        result = symbol
+    elif symbol == "forall":
+        block, p_in = parse(expr[2])
+        if p_in:
+            result = "{}({})".format(handle_forall(expr[1]), block)
+        else:
+            result = handle_forall(expr[1])+block
+    elif symbol in infix_symbols:
+        block1, p_in1 = parse(expr[1])
+        block2, p_in2 = parse(expr[2])
+        if p_in1 and infix_symbols[symbol]:
+            block1 = "(" + block1 + ")"
+        if p_in2 and infix_symbols[symbol]:
+            block2 = "(" + block2 + ")"
+        result = "{} {} {}".format(block1, symbol, block2)
+        p_outside = True
+    else:
+        result = "{}({})".format(symbol, parse_terms(expr[1:]))
+    return result, p_outside
+
+
+if __name__ == "__main__":
+    import sys
+    print(f"Args: {sys.argv}")
+    input_smt = sys.stdin.read()
+    print("hi")
---
+++ smt2tex/smt2tex.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    print(f"Args: {sys.argv}")
+    input_smt = sys.stdin.read()
+    print("hi")
