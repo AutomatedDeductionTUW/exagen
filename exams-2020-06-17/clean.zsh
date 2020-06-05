2810877860bf6b61d2abfc97f40bd8534cb243b8
Jakob Rath <git@jakobrath.eu>
2020-06-05 12:17:12 +0200

    Make scripts more robust

--- exams-2020-06-17/clean.zsh
+++ exams-2020-06-17/clean.zsh
@@ -3,5 +3,12 @@
 # Abort on errors
 set -euo pipefail
 
+setopt extendedglob
 
-rm **/*.aux **/*.fdb_latexmk **/*.fls **/*.log **/*.out **/*.pdf **/*.synctex.gz **/*.smt2.tex
+# Absolute path of directory containing the current script
+# Modifiers: P -> absolute path; h -> parent directory
+exams_dir="${${(%):-%x}:P:h}"
+
+for file in "$exams_dir"/**/*.(aux|fdb_latexmk|fls|log|out|pdf|synctex.gz|smt2.tex)(#qN); do
+    rm -- "$file"
+done
--- exams-2020-06-17/compile.zsh
+++ exams-2020-06-17/compile.zsh
@@ -3,12 +3,45 @@
 # Abort on errors
 set -euo pipefail
 
-SMT2TEX="../smt2tex/smt2tex.py"
+setopt extendedglob
 
-rm final/*.pdf
+# Absolute path of directory containing the current script
+# Modifiers: P -> absolute path; h -> parent directory
+exams_dir="${${(%):-%x}:P:h}"
 
-for exam in exam-*; do
+# Root directory of repository
+root_dir="${exams_dir:h}"
+
+num_exams="3"   # Number of exams to generate
+seed="123"      # Pseudo-random number generator seed
+
+SMT2TEX="${root_dir}/smt2tex/smt2tex.py"
+
+for file in "$exams_dir"/final/*.pdf(#qN); do
+    rm -- "$file"
+done
+
+# Generate SAT problems
+( cd "${root_dir}/generator" &&
+    stack run -- -n "$num_exams" -s "$seed" -o "$exams_dir" sat )
+
+# Generate SMT problems
+( cd "${root_dir}/generator" &&
+    stack run -- -n "$num_exams" -s "$seed" -o "$exams_dir" smt -t "${root_dir}/problems/smt/template1.smt2" )
+
+# Generate ground superposition problems
+# TODO
+# ( cd "${root_dir}/generator" &&
+#     stack run -- -n "$num_exams" -s "$seed" -o "$exams_dir" sup )
+
+# Generate redundancy problems
+# TODO
+# ( cd "${root_dir}/generator" &&
+#     stack run -- -n "$num_exams" -s "$seed" -o "$exams_dir" red )
+
+for exam in "$exams_dir"/exam-*; do
     echo "Compiling $exam"
+    examtex="${exam:t}.tex"
 
     # Produce latex version of smt files
     for smtfile in "$exam"/*.smt2; do
@@ -16,14 +49,14 @@ for exam in exam-*; do
     done
 
     # Link the template from parent directory
-    ln -sf "../template.tex" "${exam}/${exam}.tex"
+    ln -sf "../template.tex" "${exam}/${examtex}"
 
     # Compile exam
-    ( cd "$exam" && latexmk -quiet -pdf "${exam}.tex" )
+    ( cd "$exam" && latexmk -quiet -pdf "$examtex" )
 done
 
-mkdir -p final
-cp exam-*/exam-*.pdf final/
+mkdir -p "${exams_dir}/final"
+cp "$exams_dir"/exam-*/exam-*.pdf "${exams_dir}/final/"
 
 echo
 echo "Compiled exams are in directory 'final'"
