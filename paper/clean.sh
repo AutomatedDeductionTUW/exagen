0e0b214ed23d125e213a52cdb6060c626ba6ee2f
Jakob Rath <git@jakobrath.eu>
2020-06-10 18:40:05 +0200

    Add paper stub

---
+++ paper/clean.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+#
+# clean.sh
+# Copyright (C) 2020 Jakob Rath <git@jakobrath.eu>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+trash *.aux *.bbl *.blg *.fdb_latexmk *.fls *.log *.out *.synctex.gz
+trash main.pdf
